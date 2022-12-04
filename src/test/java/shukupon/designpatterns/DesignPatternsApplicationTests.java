83998e6953dea8d25e36592dbe6853a27fe93534
Shukupon <akiraiwa84@gmail.com>
2022-12-04 12:17:33 +0900

    first commit

---
+++ src/test/java/shukupon/designpatterns/DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package shukupon.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
