140a96bc24c1e8a2e47d72050bffcbb4ef105c48
Shukupon <akiraiwa84@gmail.com>
2023-02-18 20:06:25 +0900

    adapter patternを微修正

--- src/main/java/shukupon/designpatterns/adapter/Main.java
+++ src/main/java/shukupon/designpatterns/adapter/Main.java
@@ -14,7 +14,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		// 委譲を使ったadapter pattern
-		LiquorStock liquorStock = new LiquorStock(
+		Stock liquorStock = new LiquorStock(
 				new ArrayList<String>(Arrays.asList("Wine", "Whiskey", "Sake", "Shochu")));
 		
 		// MainクラスはLiquorShelfを知らずに利用できる
